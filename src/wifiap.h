52203db25ed4779708527c1b8149013802e76116
Gordon McLellan <gordonthree@gmail.com>
2017-11-03 11:03:01 -0400

    oops

--- src/wifiap.h
+++ src/wifiap.h
@@ -2,9 +2,9 @@
 #define _GLM_WIFIAP_H
 
 #define _GLM_WIFI_AP1 "Tell my wifi I love her"
-#define _GLM_WIFI_PW1 "2317239216"
+#define _GLM_WIFI_PW1 "xxxxxxxxxx"
 
 #define _GLM_WIFI_AP2 "DXtrailer"
-#define _GLM_WIFI_PW2 "2317239216"
+#define _GLM_WIFI_PW2 "xxxxxxxxxx"
 
 #endif
